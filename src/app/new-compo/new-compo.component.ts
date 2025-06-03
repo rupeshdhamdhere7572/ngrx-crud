ee11d3e4fa5458bc68ac7160e17e72c1fcc1b471
rupeshdhamdhere7572 <dhamdhere.rupesh@gmail.com>
2025-06-03 12:33:10 +0530

    new component

---
+++ src/app/new-compo/new-compo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-compo',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-compo.component.html',
+  styleUrl: './new-compo.component.css'
+})
+export class NewCompoComponent {
+
+}
